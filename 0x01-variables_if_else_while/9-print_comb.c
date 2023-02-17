7cf1af6e25a47bb49a65b3b0699fbbe4b86302b1
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-02-17 21:32:17 +0300

    patience

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	int a = 48;
+
+	do {
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		a++;
+	} while (a != 58);
+	putchar('\n');
+	return (0);
+}
