8757300adcd40cafbef80c51e7dc7cdd85c92318
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-02-21 21:05:29 +0300

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - natural numbers from n to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	int cur;
+
+	for (cur = n; cur != 98; cur < 98 ? cur++ : cur--)
+	{
+		printf("%i, ", cur);
+	}
+	printf("98\n");
+}
