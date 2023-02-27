3416e0d1faae27ab4a3ad65692b6d6995b30c384
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-02-27 23:32:06 +0300

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int len = 0;
+	char temp[1000];
+
+	for (i = 0; *(s + i) != '\0'; i++)
+		len++;
+
+	for (i = 0; i < len; i++)
+		temp[i] = *(s + (len - i - 1));
+
+	for (i = 0; i < len; i++)
+		*(s + i) = temp[i];
+}
