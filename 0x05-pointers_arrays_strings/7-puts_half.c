7ccb357bd58b438f123706bf94de71211c9240f4
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-02-27 23:35:58 +0300

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: processed string
+ */
+
+void puts_half(char *str)
+{
+	int i, start;
+	int len = 0;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+	{
+		len++;
+	}
+	len++;
+	if (len % 2 != 0)
+		start = (len - 1) / 2;
+	else
+		start = len / 2;
+	for (i = start; *(str + i) != '\0'; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
