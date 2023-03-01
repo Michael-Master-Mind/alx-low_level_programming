f90fb4fe3a46aee06ebd6e3ab5f6ec3e8db7b321
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-03-01 20:24:09 +0300

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * string_toupper - changes string to uppercase
- * @s: string to be changed
+ * cap_string - capitalizes the first letter of all words in the string
+ * @s: string
  *
- * Return: char pointer to changed string
+ * Return: char pointer to the updated string
  */
 
-char *string_toupper(char *s)
+char *cap_string(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] -= 32;
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] -= 32;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] == ' ' || s[i] == '\n' || s[i] == '\t'
+		    || s[i] == ',' || s[i] == ';' || s[i] == '.'
+		    || s[i] == '!' || s[i] == '?' || s[i] == '"'
+		    || s[i] == '(' || s[i] == ')' || s[i] == '{'
+		    || s[i] == '}')
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+		i++;
+	}
 	return (s);
 }
-
