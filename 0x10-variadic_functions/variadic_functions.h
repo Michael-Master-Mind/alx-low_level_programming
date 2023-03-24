0d6b8a0f2964f846f2ba6843f9f529f9b9869943
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-03-24 21:47:36 +0300

    .h file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef var_fun
+#define var_fun
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
