b165a573182ded503edd1b59697381c50febe0e1
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-03-30 21:07:51 +0300

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void premain(void) __attribute__((constructor));
+
+/**
+ * premain - executes before main
+ */
+void premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
