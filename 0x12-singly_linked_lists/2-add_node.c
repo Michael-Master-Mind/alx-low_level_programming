dc216a80241985833dd89c79746b1513d354079e
Michael Demissew AB <michaeldemissew@yahoo.com>
2023-03-30 21:02:18 +0300

    task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a linked list
+ * @head: pointer to the pointer to the first node
+ * @str: string value of the node
+ *
+ * Return: address of the new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
+
